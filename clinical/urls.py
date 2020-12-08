d05e5b3d1d091bbeb660d10ef55d7ff59358b7b6
DENNIS VAZ <jataiense@gmail.com>
2020-12-08 09:40:24 -0300

    Updating preconsults

--- clinical/urls.py
+++ clinical/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -15,4 +17,4 @@ urlpatterns = [
     path('auditoria/', include('audit.urls')),
     path('postagens/', include('posts.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
