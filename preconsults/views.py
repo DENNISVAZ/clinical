1b9e1a5dbe5754f969355c3df7071c2a429b0a23
DENNISVAZ <jataiense@gmail.com>
2020-12-10 07:46:28 -0300

    Implementing research in listpreconsultas

--- preconsults/views.py
+++ preconsults/views.py
@@ -3,6 +3,10 @@ from django.contrib import auth, messages
 from django.contrib.auth.models import User
 from .models import Preconsult
 from django.contrib.auth.decorators import login_required
+from datetime import date, timedelta
+from django.utils.dateformat import DateFormat
+from django.db.models import Q
+from django.core.paginator import Paginator
 
 
 def preconsults(request):
@@ -38,10 +42,38 @@ def preconsults(request):
 
 @login_required(login_url='index')
 def listpreconsults(request):
+    if request.method != 'POST':
+        # page = request.GET.get('pg')
+        first_date = request.GET.get('first_date', date.today () - timedelta ( days=1 ))
+        last_date = request.GET.get('last_date', date.today ())
+        first_date = DateFormat ( first_date ).format ( 'yy-m-d' )
+        last_date = DateFormat ( last_date ).format ( 'yy-m-d' )
+        search = ""
+    else:
+        first_date = request.POST.get('first_date')
+        last_date = request.POST.get('last_date')
+        search = request.POST.get ( 'search' )
+        page = request.GET.get('pg')
+
+
+    if first_date > last_date:
+        messages.error(request, 'Data Inicial deve ser anterior a data Final.')
     data = Preconsult.objects.order_by('-creation_time').filter(
+        Q(name__icontains=search)|Q(phone__icontains=search)|Q(profession__icontains=search)|Q(surgery__icontains=search),
+        creation_date__lte = last_date,
+        creation_date__gte = first_date,
         active = True
     )
-    return render(request, 'preconsults/list_preconsultas.html', {'dados': data})
+    if not data:
+        messages.error(request, 'Dados não encontrados para esse período e pesquisa.')
+    # paginator = Paginator(data, 5)
+    # data = paginator.get_page(page)
+
+    return render(request, 'preconsults/list_preconsultas.html', {'dados': data,
+                                                                  'first_date': first_date,
+                                                                  'last_date': last_date,
+                                                                  'search' : search,
+                                                                  })
 
 
 @login_required(login_url='index')
